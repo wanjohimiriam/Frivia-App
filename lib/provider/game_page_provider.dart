2b790fa89c3199de60dc2c52ab89dac28d8bd409
Dev_wanjohi <92452113+wanjohimiriam@users.noreply.github.com>
2023-05-30 15:21:05 +0300

    Getting Trivia Quizes from API

--- lib/provider/game_page_provider.dart
+++ lib/provider/game_page_provider.dart
@@ -1,15 +1,25 @@
- 
- 
- import 'package:dio/dio.dart';
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class GamePageProvider extends ChangeNotifier{
+class GamePageProvider extends ChangeNotifier {
   final Dio _dio = Dio();
+  int maxQuestions = 10;
 
+  List? questions;
   BuildContext context;
-  GamePageProvider({required this.context}){
-  _dio.options.baseUrl = "https://opentdb.com/api.php";
-  print("hello");
+  GamePageProvider({required this.context}) {
+    _dio.options.baseUrl = "https://opentdb.com/api.php";
+    _openQuestionsFromAPI();
   }
- }
- 
+
+  Future<void> _openQuestionsFromAPI() async {
+    var _response = await _dio.get(
+      '',
+      queryParameters: {'amount': 10, 'type': "bolean", 'difficulty': 'easy'},
+    );
+    var _data = jsonDecode(_response.toString());
+    questions= _data["results"];
+  }
+}
