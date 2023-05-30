bcf8a49106101dcf0f5d2dc6666a370f52a44cbd
Dev_wanjohi <92452113+wanjohimiriam@users.noreply.github.com>
2023-05-30 12:05:09 +0300

    UI material Buttons

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -2,11 +2,21 @@
 
 import 'package:flutter/material.dart';
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
+
   const GamePage({super.key});
 
   @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  double? _devWidth, _devHeight;
+
+  @override
   Widget build(BuildContext context) {
+    _devHeight= MediaQuery.of(context).size.height;
+    _devWidth= MediaQuery.of(context).size.width;
     return _Good_UI();
   }
 
@@ -26,17 +36,56 @@ class GamePage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         _textUI(),
+        Column(
+          children: [
+            _trueButton(),
+            SizedBox(
+              height: _devHeight! *0.01,
+            ),
+            _falseButton(),
+          ],
+        )
       ],
     );
   }
+
   Widget _textUI() {
-    return Text(
-      "Test Question 1 nothing Interensting",
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 25,
-        fontWeight: FontWeight.w400
+    return Center(
+      child: Text(
+        "Test Question 1 nothing Interensting",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.w400
+        ),
       ),
     );
+  }
+
+  Widget _trueButton(){
+    return MaterialButton(
+      onPressed: (){},
+      color: Colors.green,
+      minWidth: _devWidth! * 0.80,
+      height: _devHeight! *0.10,
+      child: Text("True",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25
+      ),),
+      );
+  }
+  Widget _falseButton(){
+    return MaterialButton(
+      onPressed: (){},
+      color: Colors.red,
+      minWidth: _devWidth! * 0.80,
+      height: _devHeight! *0.10,
+      child: Text("False",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25
+      ),),
+      );
   }
 }
