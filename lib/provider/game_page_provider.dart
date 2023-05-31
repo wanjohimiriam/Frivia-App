048c7eb745b2625ab453e2fff38c5e08051bf623
Dev_wanjohi <92452113+wanjohimiriam@users.noreply.github.com>
2023-05-31 11:07:15 +0300

    Alert Dialogue Widget

--- lib/provider/game_page_provider.dart
+++ lib/provider/game_page_provider.dart
@@ -8,36 +8,51 @@ import 'package:flutter/material.dart';
 class GamePageProvider extends ChangeNotifier {
   final Dio _dio = Dio();
   int maxQuestions = 10;
-  int _currentQuestions= 0;
+  int _currentQuestions = 0;
 
   List? questions;
-  
+
   BuildContext context;
   GamePageProvider({required this.context}) {
     _dio.options.baseUrl = "https://opentdb.com/api.php?";
     _openQuestionsFromAPI(10, "easy", "boolean");
   }
 
-  Future<void> _openQuestionsFromAPI(int amount, String difficulty, String type) async {
+  Future<void> _openQuestionsFromAPI(
+      int amount, String difficulty, String type) async {
     var _response = await _dio.get(
       'https://opentdb.com/api.php?amount=$amount&difficulty=$difficulty&type=$type',
       // queryParameters: {'amount': 10, 'difficulty': 'easy','type': "bolean"},
     );
     var _data = (_response.data as Map);
     // print("Data is: $_data");
-    questions= _data["results"];   
+    questions = _data["results"];
     debugPrint("Questions are: $questions");
     notifyListeners();
   }
-  String getCurrentQuestionsAPI(){
+
+  String getCurrentQuestionsAPI() {
     //debugPrint("Questions are: $questions");
     return questions![_currentQuestions]['question'] ?? "loading...";
   }
 
-  void answerCurrentQuestions( String _answer) async{
-    bool isCorrect = questions![_currentQuestions]['correct_answer']== _answer;
+  void answerCurrentQuestions(String _answer) async {
+    bool isCorrect = questions![_currentQuestions]['correct_answer'] == _answer;
     _currentQuestions++;
-    print(isCorrect ? "Correct" : "Incorrect");
+    //print(isCorrect ? "Correct" : "Incorrect");
+    showDialog(
+        context: context,
+        builder: (BuildContext _context) {
+          return AlertDialog(
+            backgroundColor: isCorrect ? Colors.green : Colors.red,
+            title: Icon(
+              isCorrect ? Icons.check_circle : Icons.cancel_sharp,
+              color: Colors.white,
+            ),
+          );
+        });
+    await Future.delayed(const Duration(seconds: 1));
+    Navigator.pop(context);
     notifyListeners();
-}
+  }
 }
